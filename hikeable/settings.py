27bdc46516d9dbb6fe61a6f3333e8e2d9043cb55
haruna <haruna.kawakami867@gmail.com>
2022-12-09 16:17:51 +0900

    feat: successfully seed data to database

--- hikeable/settings.py
+++ hikeable/settings.py
@@ -139,3 +139,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+# Fixutures
+FIXTURE_DIRS = [
+    'traildata',
+]
